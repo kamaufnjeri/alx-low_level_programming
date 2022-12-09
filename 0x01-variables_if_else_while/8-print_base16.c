c630450baa000297b855ba06c8d7980d05a00284
kamaufnjeri <kamaufnjeri2019@gmail.com>
2022-12-09 09:38:58 -0800

    hexadecimal task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry points
+ *
+ * Rturn: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	i = 0;
+	c = 'a';
+
+	while (i <= 9)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	do {
+		putchar(c);
+		c++;
+	} while (c <= 'f');
+	putchar('\n');
+	return (0);
+}
