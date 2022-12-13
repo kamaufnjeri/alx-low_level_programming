ff32eeabd883ab844b98316f42e3dfb98ab7282c
kamaufnjeri <kamaufnjeri2019@gmail.com>
2022-12-13 08:39:38 -0800

    lower case alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabets in lowecase
+ *
+ */
+
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 #define MAIN_H_
 
 int _putchar(char c);
+void print_alphabet(void);
 #endif
