06bff35e52d3c1575b4e81ca5d25cfc0beb27e58
kamaufnjeri <kamaufnjeri2019@gmail.com>
2022-12-13 10:55:18 -0800

    sign task5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of a number
+ * @n: number to print sign
+ * Return: 1 if greater than zero, 0 if  zero and -1 iof less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
