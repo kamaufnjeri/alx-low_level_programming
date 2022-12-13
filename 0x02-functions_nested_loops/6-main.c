afe6927e8429d2ab7be3e3b0a2ffc53d05795054
kamaufnjeri <kamaufnjeri2019@gmail.com>
2022-12-13 11:46:13 -0800

    absolute numebr

---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check youur code
+ * Return: always zero
+ */
+
+int main(void)
+{
+	int r;
+
+	r = _abs(-1);
+	printf("%d\n", r);
+	r = _abs(0);
+	printf("%d\n", r);
+	r = _abs(1);
+	printf("%d\n", r);
+	r = _abs(-98);
+	printf("%d\n", r);
+	return (0);
+}
