ab5661f06186b4ac547ca8668ac2cc95f481887b
kamaufnjeri <kamaufnjeri2019@gmail.com>
2022-12-13 23:53:04 -0800

    the times table from 0 to 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table -n the timetable of nine
+ */
+
+void times_table(void)
+{
+	int r, c, d;
+
+	for (r = 0; r <= 9; r++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (c = 0; c <= 9; c++)
+		{
+			d = (r * c);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+
+			if (c < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
