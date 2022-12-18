a2c3bcae3522fee738a0bd08281c7ca0fe303651
kamaufnjeri <kamaufnjeri2019@gmail.com>
2022-12-18 06:05:49 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long int i;
+	unsigned long int a = 1;
+	unsigned long int b = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int b1;
+	unsigned long int b2;
+	unsigned long int a1;
+	unsigned long int a2;
+
+	printf("%lu,", b);
+
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %lu", a);
+		a += b;
+		b = a - b;
+	}
+	b1 = (b % l);
+	b2 = (b / l);
+	a1 = (a / l);
+	a2 = (a % l);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", a1 + (a2 / l));
+		printf("%lu", a2 % l);
+		a1 = a1 + b1;
+		b1 = a1 + b1;
+		a2 = a2 + b2;
+		b2 = a2 + b2;
+	}
+	printf("\n");
+	return (0);
+}
