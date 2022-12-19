b2eb6aa809de90e9684be782b3319417193f2835
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2022-12-19 17:00:51 +0300

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,1 +1,39 @@
-hs
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+/**
+ * main - print password.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int ascii = 2772, a = 0, b, random;
+	char password[100];
+	time_t t;
+
+	srand((int) time(&t));
+	while (ascii > 126)
+	{
+		random = rand() % 126;
+		password[a] = random;
+		ascii -= random;
+		a++;
+	}
+	if (ascii > 0)
+		password[a] = ascii;
+	else
+	{
+		a--;
+	}
+	
+
+	for (b = 0; b <= a; b++)
+	{
+		printf("%c", password[b]);
+	}
+	return (0);
+}
