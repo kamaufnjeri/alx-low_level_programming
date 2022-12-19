8c594f0dc1deeabafed3863be0df52a8ef57975e
kamaufnjeri <kamaufnjeri2019@gmail.com>
2022-12-19 06:26:31 -0800

    task1 debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,7 +13,7 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-	
+
 	/**
 	* while (i < 10)
 	* {
@@ -22,5 +22,5 @@ int main(void)
 	*/
 	printf("Infinite loop avoided! \\o/\n");
 
-	return 0;
+	return (0);
 }
