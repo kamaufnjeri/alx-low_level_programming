699e7360cc7ec70410ca0542609b89cfb2998a9b
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2022-12-19 17:32:20 +0300

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,13 +7,12 @@
 
 int main(void)
 {
-
 	int i;
 
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
+	
 	/**
 	* while (i < 10)
 	* {
@@ -21,6 +20,5 @@ int main(void)
 	*}
 	*/
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
