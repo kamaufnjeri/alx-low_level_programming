b29c84c70d33160137e5a033dce5d97061f27add
kamaufnjeri <kamaufnjeri2019@gmail.com>
2022-12-19 03:29:30 -0800

    task2 string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.>
+
+/**
+ * _strlen - length of string
+ * @s: string pointer
+ * Return: return length of string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	for (; *s != '\0'; s++)
+	{
+		len += 1;
+	}
+	return (0);
+}
