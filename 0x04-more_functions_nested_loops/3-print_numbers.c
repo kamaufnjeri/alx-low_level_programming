32f0bc23c40d572bd7d0458dc000c1806a8325b2
kamaufnjeri <kamaufnjeri2019@gmail.com>
2022-12-20 00:13:11 -0800

    print numbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print no 0-9
+ */
+void print_numbers(void)
+{
+
+	int a;
+
+	for (a = 0; a <= 9;; a++)
+	{
+		_putchar(a + '0');
+	}
+	_putchar('\n');
+}
