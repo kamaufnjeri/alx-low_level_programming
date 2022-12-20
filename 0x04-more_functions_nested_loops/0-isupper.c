045737bdf64cccf412b154324632efe0f4bf31bb
kamaufnjeri <kamaufnjeri2019@gmail.com>
2022-12-19 22:50:25 -0800

    task0 check for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - check for uppercase character
+ * @c: character to be checked
+ * Return: 1 or c or 0
+ */
+int _isupper(int c)
+{
+char upper[26];
+
+upper[26] = {case 'A' ... 'Z':};
+if (c == upper)
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
