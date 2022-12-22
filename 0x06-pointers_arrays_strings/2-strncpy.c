5f46631599131a10ed60b119ca38eade8d7e30a3
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2022-12-22 08:23:18 +0300

    task2 copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @dest: string to copy
+ * @src: string
+ * @n: size of second string
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		*(dest + i) = *(src + i);
+	for ( ; i < n; i++)
+		*(dest + i) = '\0';
+	return (dest);
+}
