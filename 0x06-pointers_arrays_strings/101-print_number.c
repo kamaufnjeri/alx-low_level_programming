4dee2638e1bebf6e0f0053d08153b83173960926
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2022-12-22 10:06:05 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,14 +6,18 @@
  */
 void print_number(int n)
 {
+	unsigned int x;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	x = n;
 
-	_putchar((n % 10) + '0');
+	if (x / 10)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
 }
