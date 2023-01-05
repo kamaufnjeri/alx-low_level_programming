b058bcb292480c214d65e841f91bf787e33b0fbe
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-01-05 00:13:58 -0800

    task2 - length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - length of a function
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s)
+	{
+		length++;
+		length += _strlen_recursion(s + 1);
+	}
+	return (length);
+}
