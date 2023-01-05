fe245a711aec67ea70adf83be5129eea31cc9836
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-01-05 00:24:20 -0800

    task3 - factorial of n

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * factorial - to get factorial of n
+ * @n: no to find factorial
+ * Return: -1 if n < 0 to indicate an error
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
