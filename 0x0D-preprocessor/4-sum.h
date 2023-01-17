25c5aecba3d57c395e7af905ae295dade340915e
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-01-18 00:34:28 +0300

    Update 4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,6 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-
-#define ABS(x) ((x) < (0) ? -(x) : (x))
-
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
+#define ABS(x)  (((x) < 0) ? -(x) : (x))
 #endif
