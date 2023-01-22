60bdd3852c7b96f9c85291fd3436165f2efcadb2
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-01-22 18:13:08 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,45 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-/**
- * main - function
- *
- * Return: 0
- */
-
-int main(void)
+int main()
 {
-	unsigned long int i;
-	unsigned long int a = 2;
-	unsigned long int b = 1;
-	unsigned long int l = 1000000000;
-	unsigned long int b1;
-	unsigned long int b2;
-	unsigned long int a1;
-	unsigned long int a2;
+    int a = 0;
+    long int b = 0, c = 1, next;
 
-	printf("%lu", b);
-
-	for (i = 1; i < 91; i++)
-	{
-		printf(", %lu", a);
-		a += b;
-		b = a - b;
-	}
-	b1 = (b / l);
-	b2 = (b % l);
-	a1 = (a / l);
-	a2 = (a % l);
-
-	for (i = 92; i < 99; ++i)
-	{
-		printf(", %lu", a1 + (a2 / l));
-		printf("%lu", a2 % l);
-		a1 = a1 + b1;
-		b1 = a1 - b1;
-		a2 = a2 + b2;
-		b2 = a2 - b2;
-	}
-	printf("\n");
-	return (0);
+    while (a < 98)
+    {
+        next = b + c;
+        b = c;
+        c = next;
+        printf("%lu", next);
+        if (a < 97)
+        {
+            printf(", ");
+        }a++;
+    }putchar('\n');
+    return (0);
 }
