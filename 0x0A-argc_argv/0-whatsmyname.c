cd319818e79af33ca13d55710507beaaa9dadd41
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-01-25 23:34:13 -0800

    flozzy

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
- * main - Prints the program name, followed by a new line.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: Always o.
+ * main -  a program that prints its name, followed by a new line.
+ * @argc: count of arguments
+ * @argv: arguments
+ * Return: 0
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	int count;
 
+	for (count = 0; count < argc; count++)
+	{
+		printf("%s\n", argv[count]);
+	}
 	return (0);
 }
