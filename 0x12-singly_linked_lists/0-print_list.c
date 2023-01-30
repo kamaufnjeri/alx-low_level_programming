4db8a0e5b9918a7662338ac44afd51979e9f9634
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-01-30 01:47:09 -0800

    testing task0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list
+ * @h: nodes
+ * Return: node counts
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node = 0;
+
+	for (; h != NULL; )
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+
+		h = h->next;
+		node++;
+	}
+	return (node);
+}
