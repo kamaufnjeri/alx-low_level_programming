2fdceca66413f537b339c21e42e788abc7ab77d6
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-01-30 14:14:20 +0300

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,7 +2,7 @@
 
 /*
  * list_len -  find the number of elements in a linked list_t list.
- * @*h: linked list_t
+ * @h: linked list_t
  * Return: number of elements
  */
 size_t list_len(const list_t *h)
