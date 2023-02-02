a786b2422805332313995be0353b935a96b15d50
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-02-02 15:35:21 +0300

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,12 +10,12 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int node;
-
-	if (head == NULL)
-		return (NULL);
-	if (head != NULL)
+	
+	if (head)
 		for (node = 0; node < index; node++)
 		{
+			if (head == NULL)
+				return (NULL);
 			head = head->next;
 		}
 	return (head);
