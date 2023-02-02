b20bf005c15b0ec4482d280a915601c7a9c90daa
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-02-02 15:46:49 +0300

    Create 8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+/*#include "lists.h"
+
+/**
+ * sum_listint - Calculates sum of all data (n) of listint_t list.
+ * @head: A pointer to head of listint_t list.
+ *
+ * Return: If list is empty 0.Otherwise - sum of all data.
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	for (sum = 0; head; sum += head->n)
+  {
+		head = head->next;
+	}
+  return (sum);
+}
