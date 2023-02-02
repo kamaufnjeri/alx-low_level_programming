e4689da6982f78319d6693c499e16f38bc05f26f
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-02-02 14:46:43 +0300

    Create 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * free_listint2 - frees a listint_t list
+  * @head: pointer to the address of head of listint_t list
+  *
+  * Return: set head to NULL
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = (*head);
+		*head = (*head)->next;
+		free(temp);
+	}
+	
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	free(*head);
+	head = NULL;
+}
