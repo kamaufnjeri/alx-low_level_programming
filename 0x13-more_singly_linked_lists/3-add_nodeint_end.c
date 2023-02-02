4c5529597a2906eb96c5161da7e2164bcc3a2c96
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-02-02 13:57:06 +0300

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -20,7 +20,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	if (*head == NULL)
 		*head = newadd;
-
+		return (*head);
 	else
 	{
 		temp = *head;
@@ -29,6 +29,4 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		temp->next = newadd;
     return (temp);
 	}
-
-	return (*head);
 }
