a637f073fdaf368ca5bec7df6d4967081a53c28a
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-02-02 12:40:39 +0300

    Create 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  * listint_len - Counts the elements in a linked list
+  * @h: The head of the linked list
+  *
+  * Return: The number of elements in a linked list
+  */
+size_t listint_len(const listint_t *h)
+{
+	size_t node;
+
+	for (node = 0; h != NULL; node++)
+  {
+    h = h->next;
+  }
+
+	return (node);
+}
