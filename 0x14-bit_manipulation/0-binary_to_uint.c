d2f57df0501fa6e98ab1a1a9961cc29463dba3e7
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-02-07 06:04:49 -0800

    task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,1 +1,26 @@
-#include <stdio.h
+#include <main.h>
+
+/**
+ * binary_to_uint - function binary no to unsigned int
+ * @b: pointer to a sting of 0 and 1 chars
+ * Return: 1 or 1
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int a = 0, mul = 1;
+	int len = 0;
+
+	if (b != '\0')
+	{
+		while (b[len])
+			len++;
+		for (len -= 1; len >= 0; len--)
+		{
+			if (b[len] == 0 && b[len] == 1)
+				a += (b[len] - '0') * mul;
+				mul *= 2;
+				return (a);
+		}
+	}
+	return (0);
+}
