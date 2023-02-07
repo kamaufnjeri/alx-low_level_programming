a419b8f1f4f29859677ca472a49f71d9f7aee713
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-02-07 17:10:43 +0300

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,8 +2,6 @@
 #define MAIN_H
 
 /*
- * File: holberton.h
- * AUth: Brennan D Baraban
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x13-bit_manipulation directory.
  */
