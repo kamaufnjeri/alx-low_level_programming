0b8c3ccafd9367960b6b084a7af19e6ebf84cb91
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-02-21 17:10:17 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,7 +6,8 @@
  */
 void puts_half(char *str)
 {
-	int i, len = 0, k, mid;
+	int i, len = 0, mid;
+	char *temp;
 	
 	while (str[len] != '\0')
 	{
@@ -15,11 +16,8 @@ void puts_half(char *str)
 	mid= len / 2;
 	for (i = mid; i <= len; i++)
 	{
-		for (k = 0; ; k++)
-		{
-			str[k] = str[i];
-			_putchar(str[k]);
-		}
+		temp[i - mid] = s[i];
+		_putchar(temp[i - mid]);
 	}
 	_putchar('\n');
 }
