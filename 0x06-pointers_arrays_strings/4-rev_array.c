36efe2f7c1d3d331dd46c0ed2bf97b431aa52af6
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-02-24 15:25:17 +0300

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,20 +10,12 @@
   */
 void reverse_array(int *a, int n)
 {
-	int *p, i, aux, k;
-
-	p = a;
-
-	for (i = 1; i < n; i++)
+	int temp, i;
+	
+	for (i = 0; i < n / 2; i++)
 	{
-		p++;
-	}
-
-	for (k = 0; k < i / 2; k++)
-	{
-		aux = a[k];
-		a[k] = *p;
-		*p = aux;
-		p--;
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
 	}
 }
