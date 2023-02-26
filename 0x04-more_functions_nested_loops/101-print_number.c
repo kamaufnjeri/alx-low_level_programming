6f95417ab54147db04b6d3b318ab41a41377042a
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-02-26 19:36:59 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,40 +7,17 @@
  */
 void print_number(int n)
 {
-	unsigned int z;
-	int m, b;
+	unsigned int num;
 
-	b = 10;
-
-	if (n < 10 && n >= 0)
+	if (n < 0)
 	{
-		_putchar (n + '0');
-	}
-	else if (n > -10 && n < 0)
-	{
-		n = n - 2 * n;
 		_putchar('-');
-		_putchar (n + '0');
+		num = n * -1;
 	}
-
-	else
+	num = n
+	if (n > 9)
 	{
-		if (n < 0)
-		{
-			n = n * -1;
-			_putchar ('-');
-		}
-		z = n;
-	while (z / b > 9)
-	{
-		b = b * 10;
+		_putchar(n / 10 + '0');
 	}
-	while (b > 0)
-	{
-		m = z / b;
-		z = z % b;
-		_putchar (m + '0');
-		b = b / 10;
-	}
-	}
+	_putchar(n % 10 + '0')
 }
