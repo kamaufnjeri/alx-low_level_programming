9573aa6ba9e704a00c189efd05a6ed6d8c5c4067
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-02-26 21:50:12 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -20,9 +20,9 @@ int main(void)
 	for (i = 0; i < 100; i++)
 	{
 		pass[i] = rand() % 78;
-		sum += (pass[i] + '0');
-		putchar(pass[i] + '0');
-		if ((2772 - sum) - '0' < 78)
+		sum += (pass[i] );
+		putchar(pass[i]);
+		if ((2772 - sum) < 78)
 		{
 			n = 2772 - sum - '0';
 			sum += n;
