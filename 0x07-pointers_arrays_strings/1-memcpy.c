53b7a6d3116e1504cd135b5c3c3ce1484796756a
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-02-28 20:05:12 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,7 +11,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0, j = 0;
 
-	while (dest[i] != '\0' && j < n && src[j] != '\0')
+	while (dest[i] != '\0' && j < n)
 	{
 		dest[i++] = src[j++];
 	}
