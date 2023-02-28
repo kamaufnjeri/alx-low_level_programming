68c23046f210a2f39c3cbf4d70d3ea064c476f47
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-02-28 23:26:52 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,7 +9,7 @@
 char *_strpbrk(char *s, char *accept)
 {
 	unsigned int j = 0;
-	/**char *f;*/
+	char *f;
 
 	while (*s != 0)
 	{
@@ -17,7 +17,8 @@ char *_strpbrk(char *s, char *accept)
 		{
 			if (*s == *(accept + j))
 			{
-				return (s);
+				f = *s;
+				return (f);
 			}
 			j++;
 		}
