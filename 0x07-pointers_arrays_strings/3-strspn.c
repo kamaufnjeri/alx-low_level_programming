f2b11d099b1340b1f85c8f0ea3ad874f92a346d5
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-02-28 21:25:28 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,29 +9,13 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, l, j, a;
-
-	i = l = 0;
-	while (s[i] != 0)
+	unsigned int i = 0, j = 0, l = 0;
+	
+	while (s[i] && accept[j])
 	{
-		j = 0;
-		a = 0;
-		while (accept[j] != 0)
-		{
-			if (s[i] != accept[j])
-			{
-				j++;
-			}
-			else if (s[i] == accept[j])
-			{
-				a++;
-				l++;
-				j++;
-			}
-		}
-		if (a == 0)
-			return (l);
 		i++;
+		j++;
+		l++;
 	}
 	return (l);
 }
