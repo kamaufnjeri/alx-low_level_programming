f252e8467f2514539d8e77e73c8b27724d2052b4
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-02-28 20:19:54 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,9 +11,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
 
-	for (a = 0; a < n; )
+	for (a = 0; a < n; a++)
 	{
-		dest[a++] = src[a++];
+		dest[a] = src[a];
 	}
 	return (dest);
 }
