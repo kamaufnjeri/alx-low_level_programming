6d9ac382a2d646b87797595ce75e171b502a02d5
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-03-01 06:30:10 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,12 +16,12 @@ char *_strstr(char *haystack, char *needle)
 
 	if (*needle == 0)
 		return (haystack);
-	while (*haystack != '\0')
+	while (*haystack)
 	{
 		string = haystack;
 		search = needle;
 
-		while (*search == *haystack && (*haystack != '\0' && *search != '\0'))
+		while (*search == *haystack)
 		{
 			haystack++;
 			search++;
