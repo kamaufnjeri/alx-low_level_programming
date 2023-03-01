d0cc16505a47a1e56f96085eb5de5190764bb5a3
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-03-01 08:55:11 +0300

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,12 +8,9 @@
  */
 int _pow_recursion(int x, int y)
 {
-	int result = x;
-
 	if (y < 0)
 		return (-1);
 	else if (y == 0)
 		return (1);
-	result *= _pow_recursion(x, y - 1);
-	return (result);
+	return (x * (x, _pow_recursion(x, y - 1);
 }
