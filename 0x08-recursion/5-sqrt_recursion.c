9a51d8176df183db8bd99d1626ea265f344941c0
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-03-01 09:48:00 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -13,13 +13,12 @@ int _sqrt_recursion(int n);
  */
 int find_sqrt(int num, int root)
 {
-	if ((root * root) == num)
+	if (root = num / 2)
 		return (root);
-
-	if (root == num / 2)
-		return (root);
-
-	return (find_sqrt(num, root + 1));
+	if (root * root == num)
+		return (root)
+	else
+		return (find_sqrt(num, root + 1);
 }
 
 /**
@@ -32,12 +31,11 @@ int find_sqrt(int num, int root)
 int _sqrt_recursion(int n)
 {
 	int root = 0;
-
 	if (n < 0)
 		return (-1);
-
+	if (n == 0)
+		return (-1);
 	if (n == 1)
 		return (1);
-
-	return (find_sqrt(n, root));
+	return (find_sqrt(num, root);
 }
