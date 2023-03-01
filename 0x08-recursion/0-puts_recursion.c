1c0800feeb0c12f9654e13254de1b732ad1e6b92
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-03-01 08:24:59 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,10 +8,12 @@
 
 void _puts_recursion(char *s)
 {
-	while (*s != '\0')
+	if (*s)
 	{
 		_putchar(*s);
-		_puts_recursion((s + 1));
+		s++;
+		_puts_recursion(s);
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
