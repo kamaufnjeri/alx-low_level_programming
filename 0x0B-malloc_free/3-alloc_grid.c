710a217c663d33e1fd222c3f914d06ac33e9c73a
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-03-02 16:44:57 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,10 +24,9 @@ int **alloc_grid(int width, int height)
 		arr[i] = malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
 		{
-			while (i >= 0)
+			for(; i < height; i++)
 			{
 				free(arr[i]);
-				i--;
 			}
 			free(arr);
 			return (NULL);
