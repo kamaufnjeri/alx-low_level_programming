7c7b6a86cac0c724de20f559406bbd6b49f4c501
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-03-02 12:56:30 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,43 +12,50 @@
  */
 int main(int argc, char *argv[])
 {
-	int cents, coins = 0;
+	int cents, coins = 0, c0 = 25;
+        int c1 = 10, c2 = 5, c3 = 2, c4 = 1;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	cents = atoi(argv[1]);
-
-	while (cents > 0)
-	{
-		coins++;
-		if ((cents - 25) >= 0)
-		{
-			cents -= 25;
-			continue;
-		}
-		if ((cents - 10) >= 0)
-		{
-			cents -= 10;
-			continue;
-		}
-		if ((cents - 5) >= 0)
-		{
-			cents -= 5;
-			continue;
-		}
-		if ((cents - 2) >= 0)
-		{
-			cents -= 2;
-			continue;
-		}
-		cents--;
-	}
-
-	printf("%d\n", coins);
-
-	return (0);
+        if (argc == 2)
+        {
+                cents = atoi(argv[1]);
+                while (cents > 0)
+                {
+                        if (cents >= c0)
+                        {
+                                coins += cents / c0;
+                                cents = cents % c0;
+                                continue;
+                        }
+                        else if (cents >= c1)
+                        {
+                                coins += cents / c1;
+                                cents = cents % c1;
+                                continue;
+                        }
+                        else if (cents >= c2)
+                        {
+                                coins += cents / c2;
+                                cents = cents % c2;
+                                continue;
+                        }
+                        else if (cents >= c3)
+                        {
+                                coins += cents / c3;
+                                cents = cents % c3;
+                                continue;
+                        }
+                        else
+                        {
+                                coins += cents / c4;
+                                cents = cents % c4;
+                        }
+                }
+                printf("%d\n", coins);
+        }
+        else
+        {
+                printf("Error\n");
+                return (1);
+        }
+        return (0);
 }
