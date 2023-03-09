86a30fe548e83a7dffdf8e14ea5a7847b58dda33
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-03-09 15:53:59 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -11,12 +11,12 @@ char **strtow(char *str);
  * @str: The string to be searched.
  *
  * Return: The index marking the end of the initial word pointed to by str.
- */
+ */ 
 int word_len(char *str)
 {
 	int index = 0, len = 0;
 
-	while (*(str + index) != ' ')
+	while (*(str + index) != ' ' || *(str + index))
 	{
 		len++;
 		index++;
