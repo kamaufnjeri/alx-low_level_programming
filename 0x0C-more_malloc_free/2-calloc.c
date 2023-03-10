5e465d9bfbec96ca7c45a3fd518b50096c773a0f
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-03-10 15:30:26 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,20 +8,19 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *p;
-	unsigned int i, d;
+        char *temp;
+        unsigned int index;
 
-	d = nmemb * size;
-	if (nmemb <= 0 || size <= 0)
-		return (NULL);
-
-	p = malloc(d);
-	if (p == NULL)
-		return (NULL);
-
-	for (i = 0; i < (d); i++)
-	{
-		p[i] = 0;
-	}
-	return (p);
+        if (nmemb == 0 || size == 0)
+        {
+                return (NULL);
+        }
+        temp = malloc(nmemb * size);
+        if (temp == NULL)
+                return (NULL);
+        for (index = 0; index < nmemb; index++)
+        {BOB
+                temp[index] = 0;
+        }
+        return (temp);
 }
