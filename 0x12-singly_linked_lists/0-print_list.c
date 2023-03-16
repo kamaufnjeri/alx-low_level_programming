6cb60d8df33cb911e1729818655cf64aadf75362
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-03-16 18:06:05 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,13 +12,13 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (h->str == NULL)
+		if (h->st == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->le, h->st);
 		}
 
 		node++;
