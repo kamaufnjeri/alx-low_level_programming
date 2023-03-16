7550b7b2a4fe31178500ee492f530069fef76be9
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-03-16 19:14:42 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,6 +18,8 @@ list_t *add_node(list_t **head, const char *str)
 
 	while (str[len] != '\0')
 		len++;
+	if (str == NULL)
+		return (NULL);
 	dupl = malloc(sizeof(list_t));
 	dupl->str = strdup(str);
 	if (dupl == NULL)
