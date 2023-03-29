d8eb68c9f840b05948b89d992629bbde49fc71d3
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-03-29 14:48:42 +0000

    update to task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	temp->n = n;
 	temp->next = NULL;
 	last = *head;
-	if (last != NULL)
+	if (*head != NULL)
 	{
 		while (node < (idx - 1))
 		{
