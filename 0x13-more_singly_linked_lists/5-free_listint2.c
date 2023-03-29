8f20aa937184ea944014f4166fbf4244efcc76d7
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-03-29 12:48:29 +0000

    free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,16 +9,17 @@
   */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *temp, *he;
 
 	if (head == NULL)
 		return;
-	while (*head)
+	he = *head;
+	while (he != NULL)
 	{
-		temp = (*head);
-		*head = (*head)->next;
+		temp = he;
+		he = temp->next;
 		free(temp);
 	}
-	free(*head);
-	head = NULL;
+	he = NULL;
 }
+
