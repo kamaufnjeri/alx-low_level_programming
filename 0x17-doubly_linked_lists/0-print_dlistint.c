7bbf16bbbc6bb06391cb928af46d2dbc30c083b8
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-05 16:08:57 +0000

    task 0 prin t elements of a bdouble linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,31 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements of a
- * dlistint_t list
- *
- * @h: head of the list
- * Return: the number of nodes
+ * print_dlistint - function to print integers in a double link
+ * @h: node with element to be printed
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count;
-
-	count = 0;
-
-	if (h == NULL)
-		return (count);
-
-	while (h->prev != NULL)
-		h = h->prev;
-
-	while (h != NULL)
+	size_t node = 0;
+	while (h)
 	{
 		printf("%d\n", h->n);
-		count++;
 		h = h->next;
+		node++;
 	}
-
-	return (count);
+	return (node);
 }
-
--- 0x17-doubly_linked_lists/README.md
+++ 0x17-doubly_linked_lists/README.md
@@ -1,1 +1,8 @@
-Double linked lists
+0x17. C - Doubly linked lists
+C
+Algorithm
+Data structure
+Write a function that prints all the elements of a dlistint_t list.
+
+Prototype: size_t print_dlistint(const dlistint_t *h);
+
