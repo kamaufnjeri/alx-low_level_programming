8ed9cb6cd6e7b5d16611b07b3b872d48f9e323cc
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-05 12:07:27 +0000

    p

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,40 +10,38 @@
  * Return: NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
-{
-	unsigned int count = 0;
-	listint_t *current_node, *new_node = malloc(sizeof(listint_t));
+{listint_t *new_node, *iter;
+	unsigned int count = 1;
 
-	if (new_node == NULL)
+	if (head)
 	{
-		return (NULL);
-	}
+		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
+			return (NULL);
 
-	new_node->n = n;
-	new_node->next = NULL;
-
-	if (idx == 0)
-	{
-		new_node->next = *head;
-		*head = new_node;
+		new_node->n = n;
+		if (idx > 0)
+		{
+			iter = *head;
+			while (iter)
+			{
+				if (count == idx)
+				{
+					new_node->next = iter->next;
+					iter->next = new_node;
+					return (new_node);
+				}
+				iter = iter->next;
+				count++;
+			}
+			if (idx > count)
+				return (NULL);
+		}
+		else
+		{
+			new_node->next = *head;
+			*head = new_node;
+		}
 		return (new_node);
 	}
-
-	current_node = *head;
-
-	while (current_node)
-	{
-
-		if (count == (idx - 1))
-		{
-			new_node->next = current_node->next;
-			current_node->next = new_node;
-			return (new_node);
-		}
-		count++;
-		current_node = current_node->next;
-	}
-	free(new_node);
-
-	return (NULL);
 }
