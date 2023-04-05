fc88fdd35ee412610dc1efe088ea9c486c51f0d8
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-05 11:43:03 +0000

    update to task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		for (node = 0; node < (idx - 1); node++)
 		{
-			last = last->next;
+			while (last->next != NULL)
+			{
+				last = last->next;
+			}
 		}
 		new->next = last->next;
 		last->next = new;
