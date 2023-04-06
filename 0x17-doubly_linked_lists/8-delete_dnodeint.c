f8c503bbfb40b415e2829014fc32ff16397c3346
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-04-06 16:23:50 +0300

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -45,8 +45,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	if ((*head)->next == NULL)
 	{
-		free(*head);
-		*head = NULL;
+		*head = (*head)->next;
+		free(temp);
+		temp = NULL;
 		return (1);
 	}
 	return (-1);
