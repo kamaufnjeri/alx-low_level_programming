62d69fe4633259931c2d7c312906d07bb05885f2
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-06 16:24:36 +0000

    update to task to delete node at an index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,54 +1,106 @@
 #include "lists.h"
 
-/**
- * delete_dnodeint_at_index - function to delete a node at a certain position
- * @head: address to pointer of first node
- * @index: variable containing position of node to delete
- * Return: 1 if deleted otherwise -1
- */
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
-{dlistint_t *temp = *head, *temp2;
-	unsigned int node;
+unsigned int length(const dlistint_t *head);
 
-	if (*head == NULL)
-		return (-1);
-	if ((*head)->next != NULL)
-	{
+/**
+* delete_first - delete first node
+* @head: address to pointer of first node
+* @index: variable containing position of node to delete
+* Return: 1 if deleted otherwise -1
+*/
+int delete_first(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp = *head;
+
 	if (index == 0)
 	{
+		if ((*head)->next == NULL)
+		{
+			free(*head);
+			*head = NULL;
+			return (1);
+		}
 		*head = (*head)->next;
 		(*head)->prev = NULL;
 		free(temp);
-		temp = NULL;
 		return (1);
 	}
-	for (node = 0; node < (index); node++)
+	return (-1);
+}
+/**
+* delete_at_index - delete node at index and at end
+* @head: pointer to pointer of nodes
+* @index: index to delete node
+* Return: 1 if successful or -1
+*/
+int delete_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp = *head;
+	unsigned int node, len;
+
+	len = length(*head);
+	if (index > (len - 1))
+		return (-1);
+	else if (index == (len - 1))
 	{
-		temp = temp->next;
-		if (temp->next == NULL)
+		temp = *head;
+		while (temp->next != NULL)
 		{
-			/**if (temp == NULL)
-				return (-1);*/
-		temp2 = temp->prev;
-		temp2->next = NULL;
-		free(temp);
-		temp = NULL;
-		return (1);
+			temp = temp->next;
 		}
-	}
-		temp2 = temp->prev;
-		temp2->next = temp->next;
-		temp->next->prev = temp2;
+		temp->prev->next = NULL;
 		free(temp);
 		temp = NULL;
 		return (1);
 	}
-	if ((*head)->next == NULL)
+	else if (index < (len - 1))
 	{
-		*head = (*head)->next;
+		temp = *head;
+		for (node = 0; node < index; node++)
+			temp = temp->next;
+		temp->prev->next = temp->next;
+		temp->next->prev = temp->prev;
 		free(temp);
 		temp = NULL;
 		return (1);
 	}
 	return (-1);
+}
+/**
+* delete_dnodeint_at_index - function to delete a node at a certain position
+*@head: address to pointer of first node
+*@index: variable containing position of node to delete
+*Return: 1 if deleted otherwise -1
+*/
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		return (delete_first(head, index));
+
+	}
+	else if (index > 0)
+	{
+		return (delete_at_index(head, index));
+	}
+	return (-1);
+}
+/**
+*length - length of nodes
+*@head: pointer to a node
+*Return: len of nodes
+*/
+unsigned int length(const dlistint_t *head)
+{
+	unsigned int len = 0;
+
+	while (head != NULL)
+	{
+		head = head->next;
+		len++;
+	}
+	return (len);
 }
