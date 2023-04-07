ae5a8f8c2e56ef8de6d6eb33694a9e3e01a73794
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-07 17:36:05 +0000

    task create a function to add element to a hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,12 +25,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	temp = malloc(sizeof(hash_node_t));
 	if (temp == NULL)
 		return (0);
-	if (temp->key == NULL)
-	{
-		free(temp);
-		return (0);
-	}
 	temp->key = strdup(key);
+	if (temp->key == NULL)
+		return (0);
 	temp->value = strdup(value);
 	temp->next = ht->array[index];
 	ht->array[index] = temp;
