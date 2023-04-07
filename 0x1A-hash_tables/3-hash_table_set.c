efb767e4ab9f5964e0eebe1d5c8333f53d9ee778
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-07 17:44:43 +0000

    task create a function to add element to a hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *temp, *current;
+	hash_node_t *temp = NULL, *current = NULL;
 
 	index = key_index((unsigned char *)key, ht->size);
 	current = ht->array[index];
