4c60a18a03665bba8b45c0144e17bc5b4004a8ea
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-07 19:23:38 +0000

    function to print elements in hash table in assorted order

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - a function print elements in sorted order in hash table
+ * @ht: hash table with elements;
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	int k = 0;
+	unsigned long int i;
+	hash_node_t *temp;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		if (temp != NULL)
+		{
+			for (; temp; )
+			{
+				if (k == 1)
+					printf(", ");
+				printf("'%s' : '%s'", temp->key, temp->value);
+			temp = temp->next;
+			k = 1;
+			}
+		}
+	}
+	printf("}\n");
+}
