a3715dc520e7b1a8c54ba307fbd53530856ee373
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-07 09:30:34 +0000

    implementation of djb2 technique

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - function implenmenting the djb2 algorithm
+ * @str: string used to generate hash value
+ * Return: hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5831;
+	while ((c = *str++))
+	{
+		hash = hash * 33 + c;
+	}
+	return (hash);
+}
