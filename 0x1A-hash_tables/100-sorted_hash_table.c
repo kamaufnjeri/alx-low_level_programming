1c9205b9a0841b71dab5e28b36f2b9d1b371715e
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-04-12 10:59:55 +0000

    sorted hash table

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -34,7 +34,7 @@ shash_table_t *shash_table_create(unsigned long int size)
  * @value: value to insert
  * Return: node created or NULL
  */
-/**shash_node_t *create_nod(shash_table_t *ht, const char *key, const char *value)
+shash_node_t *create_nod(shash_table_t *ht, const char *key, const char *value)
 {
 	shash_node_t *temp, *new;
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
@@ -62,7 +62,7 @@ shash_table_t *shash_table_create(unsigned long int size)
 	new->snext = NULL;
 	new->sprev = NULL;
 	return (new);
-}*/
+}
 /**
  * shash_table_set - set a sorted hash table
  * @ht: hashtable to insert node
@@ -71,7 +71,7 @@ shash_table_t *shash_table_create(unsigned long int size)
  * Return: node created or NULL
  */
 int shash_table_set(shash_table_t *ht, const char *key, const char *value)
-{/**
+{
 	shash_node_t *temp, *new;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
@@ -79,6 +79,8 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 	new = create_nod(ht, key, value);
 	if (ht->shead == NULL)
 	{
+		new->sprev = NULL;
+		new->sprev = NULL;
 		ht->shead = new;
 		ht->stail = new;
 		return (1);
@@ -111,77 +113,7 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 			return (1);
 		}
 	}
-	return (0);*/
-	shash_node_t *new, *tmp;
-	char *value_copy;
-	unsigned long int index;
-
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return (0);
-
-	value_copy = strdup(value);
-	if (value_copy == NULL)
-		return (0);
-
-	index = key_index((const unsigned char *)key, ht->size);
-	tmp = ht->shead;
-	while (tmp)
-	{
-		if (strcmp(tmp->key, key) == 0)
-		{
-			free(tmp->value);
-			tmp->value = value_copy;
-			return (1);
-		}
-		tmp = tmp->snext;
-	}
-
-	new = malloc(sizeof(shash_node_t));
-	if (new == NULL)
-	{
-		free(value_copy);
-		return (0);
-	}
-	new->key = strdup(key);
-	if (new->key == NULL)
-	{
-		free(value_copy);
-		free(new);
-		return (0);
-	}
-	new->value = value_copy;
-	new->next = ht->array[index];
-	ht->array[index] = new;
-
-	if (ht->shead == NULL)
-	{
-		new->sprev = NULL;
-		new->snext = NULL;
-		ht->shead = new;
-		ht->stail = new;
-	}
-	else if (strcmp(ht->shead->key, key) > 0)
-	{
-		new->sprev = NULL;
-		new->snext = ht->shead;
-		ht->shead->sprev = new;
-		ht->shead = new;
-	}
-	else
-	{
-		tmp = ht->shead;
-		while (tmp->snext != NULL && strcmp(tmp->snext->key, key) < 0)
-			tmp = tmp->snext;
-		new->sprev = tmp;
-		new->snext = tmp->snext;
-		if (tmp->snext == NULL)
-			ht->stail = new;
-		else
-			tmp->snext->sprev = new;
-		tmp->snext = new;
-	}
-
-	return (1);
+	return (0);
 }
 /**
  * shash_table_get - get a node at a certain index
